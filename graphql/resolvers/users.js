a48b57fcd915be001470676ea1645f26008e376a
taha uygun <tahauygun@gmail.com>
2020-01-31 23:32:32 -0800

    adding user mutation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,12 +1,36 @@
-const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require("../../models/User");
 
 module.exports = {
-  Mutation:{
-    register(_, args, context, info) {
+  Mutation: {
+    async register(
+      _,
+      {
+        registerInput: { username, email, password, confirmPassword }
+      },
+      context,
+      info
+    ) {
       // TODO: Validate user data
       // TODO: Make sure user doesnt already exist
       // TODO: Hash password and create an auth token
+      password: await bcrypt.hash(password, 12);
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString()
+      })
 
+      const res = await newUser.save();
+
+      const token = jwt.sign({
+        id: res.id,
+        email: res.email,
+        username: res.username
+      })
     }
   }
-}
+};
