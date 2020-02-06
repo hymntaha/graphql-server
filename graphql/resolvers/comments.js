4ab9058883cd850205c8ce2e79a38983a95284b4
taha uygun <tahauygun@gmail.com>
2020-02-05 22:39:00 -0800

    adding comment mutation

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,2 +1,27 @@
+const {UserInputError} = require('apollo-server');
 const Post = require('../../models/Post');
 
+module.exports = {
+  Mutation:{
+    createComment:async (_,{postId, body}, context)=>{
+      const user = checkAuth(context);
+      if (body.trim() === '') {
+        throw new UserInputError('Empty comment',{
+          errors: {
+            body: 'Comment body must not empty'
+          }
+        })
+      }
+      const post = await findById(postId);
+      if (post) {
+        post.comment.unshift({
+          body,
+          username,
+          createdAt: new Date().toISOString()
+        })
+        await post.save();
+        return post;
+      } else throw new UserInputError('Post not found')
+    }
+  }
+}
