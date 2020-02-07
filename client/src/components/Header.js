a7de235bb31bb041ff08247e1442e6e8ef53939a
taha uygun <tahauygun@gmail.com>
2020-02-06 21:50:41 -0800

    some css changes for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,29 +1,36 @@
 import React, { useState } from 'react'
 import { Menu } from 'semantic-ui-react'
+import {Link} from 'react-router-dom';
 
 function Header() {
-  const [activeItem, setActiveItem] = useState('');
+  const [activeItem, setActiveItem] = useState('home');
 
   const handleItemClick = (e, {name}) => setActiveItem(name);
 
     return (
-        <Menu pointing secondary>
+        <Menu pointing secondary size="massive" color='teal'>
           <Menu.Item
             name='home'
             active={activeItem === 'home'}
             onClick={handleItemClick}
+            as={Link}
+            to='/'
           />
 
           <Menu.Menu position='right'>
             <Menu.Item
               name='login'
-              active={activeItem === 'messages'}
+              active={activeItem === 'login'}
               onClick={handleItemClick}
+              as={Link}
+              to='/login'
             />
             <Menu.Item
               name='register'
               active={activeItem === 'register'}
               onClick={handleItemClick}
+              as={Link}
+              to='/register'
             />
           </Menu.Menu>
         </Menu>
