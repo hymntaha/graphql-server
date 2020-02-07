2c991a243d03808eabb30fa73430e0713a26c392
taha uygun <tahauygun@gmail.com>
2020-02-06 21:39:55 -0800

    adding components to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
-
 import './App.css'
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <Router>
