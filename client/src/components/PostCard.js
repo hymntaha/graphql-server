0cba00b0461918ecfaa1c000e1283937e6fd959f
taha uygun <tahauygun@gmail.com>
2020-02-08 15:21:48 -0800

    adding post card

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {Card,Icon,Label} from "semantic-ui-react";
-
-const PostCard = (props) => {
+import {Card, Icon, Label, Image, CardContent, Button} from "semantic-ui-react";
+import
+const PostCard = ({post: {body, createdAt, id, username, likeCount, commentCount, likes}}) => {
   return (
-    <div>
-      
-    </div>
+    <Card>
+      <Card.Content>
+        <Image floated="right" size="mini" src="https://react.semantic-ui"/>
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta>{}</Card.Meta>
+        <Card.Description>
+          {body}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content>
+        <div className="ui two buttons">
+          <Button ba>
+
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>
   );
 };
 
