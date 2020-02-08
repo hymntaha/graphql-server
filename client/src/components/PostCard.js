4b9bc5a19c9903ec43593deca1e6269ef22ecc5d
taha uygun <tahauygun@gmail.com>
2020-02-08 15:36:16 -0800

    adding card redirection to the main post

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+
 import {Card, Icon, Label, Image, CardContent, Button} from "semantic-ui-react";
 import moment from "moment";
 
@@ -8,7 +10,7 @@ const PostCard = ({post: {body, createdAt, id, username, likeCount, commentCount
       <Card.Content>
         <Image floated="right" size="mini" src="https://react.semantic-ui"/>
         <Card.Header>{username}</Card.Header>
-        <Card.Meta>{moment(createdAt).fromNow(true)}</Card.Meta>
+        <Card.Meta as={Link} to{`/posts/${id}`}>{moment(createdAt).fromNow(true)}</Card.Meta>
         <Card.Description>
           {body}
         </Card.Description>
