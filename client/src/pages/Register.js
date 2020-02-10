5e02a8336ff8522d66aba02c25296760526693fd
taha uygun <tahauygun@gmail.com>
2020-02-09 23:42:33 -0800

    adding input types

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -33,10 +33,10 @@ function Register()  {
     <div className='form-container'>
       <Form onSubmit={onSubmit} noValidate>
         <h1>Register</h1>
-        <Form.Input label="Username" placeholder="Username.." name="username" value={values.username} onChange={onChange}/>
-        <Form.Input label="Email" placeholder="Email.." name="email" value={values.email} onChange={onChange}/>
-        <Form.Input label="Password" placeholder="Password.." name="password" value={values.password} onChange={onChange}/>
-        <Form.Input label="Confirm Password" placeholder="Confirm Password.." name="confirmPassword" value={values.confirmPassword} onChange={onChange}/>
+        <Form.Input label="Username" placeholder="Username.." name="username" type="text" value={values.username} onChange={onChange}/>
+        <Form.Input label="Email" placeholder="Email.." name="email" type="email" value={values.email} onChange={onChange}/>
+        <Form.Input label="Password" placeholder="Password.." name="password" type="password" value={values.password} onChange={onChange}/>
+        <Form.Input label="Confirm Password" placeholder="Confirm Password.." type="password" name="confirmPassword" value={values.confirmPassword} onChange={onChange}/>
         <Button type="Submit" primary>Submit</Button>
       </Form>
     </div>
