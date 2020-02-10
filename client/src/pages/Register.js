ff14f780b3f540f1d5617266afc5e5812c82a692
taha uygun <tahauygun@gmail.com>
2020-02-09 23:37:49 -0800

    adding mutation and registerInput

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import {Form} from "semantic-ui-react";
+import React, {useState} from 'react';
+import {Form,Button} from "semantic-ui-react";
 
 
-const Register = () => {
+function Register()  {
   const [value,setValues] = useState([
     username: '',
     email:'',
     password: '',
     confirmPassword: '',
   ])
+
+  const onChange = (event)=>{
+    setValues({...values, [event.target.name]: event.target.value})
+  }
+  const onSubmit
   return (
     <div>
       <Form onSubmit={onSubmit} noValidate>
@@ -17,10 +22,28 @@ const Register = () => {
         <Form.Input label="Email" placeholder="Email.." name="email" value={values.email} onChange={onChange}/>
         <Form.Input label="Password" placeholder="Password.." name="password" value={values.password} onChange={onChange}/>
         <Form.Input label="Confirm Password" placeholder="Confirm Password.." name="confirmPassword" value={values.confirmPassword} onChange={onChange}/>
-
+        <Button type="Submit" primary>Submit</Button>
       </Form>
     </div>
   );
 };
+
+const REGISTER_USER = gql`
+mutation register(
+    $username: String!
+    $email: String!
+    $password: String!
+    $confirmPassword: String!
+){
+    register(
+        registerInput: {
+        username: $username
+        email:$email
+        password: $password
+        confirmPassword: $confirmPassword}
+    ){
+        id email username createdAt token
+    }
+}`
 
 export default Register;
