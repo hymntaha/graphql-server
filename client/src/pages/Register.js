f182b282e7c50a3c9aec3f4e2d24cc00f3dd0d36
taha uygun <tahauygun@gmail.com>
2020-02-09 23:21:08 -0800

    adding registration form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Form} from "semantic-ui-react";
+
 
 const Register = () => {
+  const [value,setValues] = useState([
+    username: '',
+    email:'',
+    password: '',
+    confirmPassword: '',
+  ])
   return (
     <div>
-      <h1>Register Page</h1>
+      <Form onSubmit={onSubmit} noValidate>
+        <h1>Register</h1>
+        <Form.Input label="Username" placeholder="Username.." name="username" value={values.username} onChange={onChange}/>
+        <Form.Input label="Email" placeholder="Email.." name="email" value={values.email} onChange={onChange}/>
+        <Form.Input label="Password" placeholder="Password.." name="password" value={values.password} onChange={onChange}/>
+        <Form.Input label="Confirm Password" placeholder="Confirm Password.." name="confirmPassword" value={values.confirmPassword} onChange={onChange}/>
+
+      </Form>
     </div>
   );
 };
