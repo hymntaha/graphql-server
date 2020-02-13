882edffd868fa98e645b1584519d481bd59056de
taha uygun <tahauygun@gmail.com>
2020-02-12 22:28:51 -0800

    refactoring logn

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,18 +3,18 @@ import { Form, Button } from "semantic-ui-react";
 import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
+import {useForm} from "../util/hooks";
+
 function Register(props) {
   const [errors, setErrors] = useState({});
-  const [values, setValues] = useState({
+
+  const {onChange,onSubmit, values} = useForm(registerUser,{
     username: "",
     email: "",
     password: "",
     confirmPassword: ""
-  });
+  })
 
-  const onChange = event => {
-    setValues({ ...values, [event.target.name]: event.target.value });
-  };
 
   const [addUser, { loading }] = useMutation(REGISTER_USER, {
     update(_, result) {
@@ -28,10 +28,11 @@ function Register(props) {
       username: values
     }
   });
-  const onSubmit = event => {
-    event.preventDefault();
+
+  function registerUser(){
     addUser();
-  };
+  }
+
   return (
     <div className="form-container">
       <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
