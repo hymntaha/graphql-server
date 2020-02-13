fbe56ed55612ad160bb7d108a554259b69e46043
taha uygun <tahauygun@gmail.com>
2020-02-12 22:38:19 -0800

    creating auth context

---
+++ client/src/context/auth.js
@@ -0,0 +1,28 @@
+import React, {useReducer, createContext} from "react";
+
+const AuthContext = createContext({
+  user: null,
+  login: (data)=>{},
+  logout: ()=>{}
+})
+
+function authReducer(state,action){
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        user: action.payload
+      }
+    case 'LOGOUT':
+      return {
+        ...state,
+        user:null
+      }
+    default:
+      return state;
+  }
+}
+
+function AuthProvider(props) {
+  
+}
