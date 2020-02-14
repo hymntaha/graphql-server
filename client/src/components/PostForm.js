d69382b82eb071f5d9fa2ff55367e80b17395c11
taha uygun <tahauygun@gmail.com>
2020-02-13 23:29:17 -0800

    creating create post functionality

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,18 +1,24 @@
 import React from "react";
-import {Form} from "semantic-ui-react";
-import {useForm} from "../util/hooks";
+import { Form, Button } from "semantic-ui-react";
+import { useForm } from "../util/hooks";
 
 function PostForm() {
-  return(
+  return (
     <Form onSubmit={onSubmit}>
       <h2>Create a post:</h2>
       <Form.Field>
-        <Form.Input>
-
-        </Form.Input>
+        <Form.Input
+          placeholder="Hi World!"
+          name="body"
+          onChange={onChange}
+          value={values.body}
+        />
+        <Button type="submit" color="teal">
+          Submit
+        </Button>
       </Form.Field>
     </Form>
-  )
+  );
 }
 
-export default PostForm
+export default PostForm;
