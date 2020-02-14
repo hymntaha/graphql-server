c9403c103ce9143508eb5aae79d30f50911be68b
taha uygun <tahauygun@gmail.com>
2020-02-13 22:58:56 -0800

    adding auth route to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { Container } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 import { AuthProvider } from "./context/auth";
-
+import AuthRoute from './util/AuthRoute';
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
@@ -18,8 +18,8 @@ function App() {
         <Container>
           <Header />
           <Router exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
         </Container>
       </Router>
     </AuthProvider>
