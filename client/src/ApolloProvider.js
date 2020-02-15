06fe7dad69659c06eefb53bf5c3bf0f33c573dca
taha uygun <tahauygun@gmail.com>
2020-02-14 23:30:37 -0800

    quick fix for apollo provider

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -9,18 +9,18 @@ import {setContext} from 'apollo-link-context';
 const httpLink = createHttpLink({
   uri:'http://localhost:5000'
 })
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-})
-
 const autLink = setContext(()=>{
   const token = localStorage.getItem('jwtToken');
   return{
     headers: {Authorization:token ? `Bearer ${token}`: ''}
   }
 })
+
+const client = new ApolloClient({
+  link: autLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
 
 export default (
   <ApolloProvider client={{client}}>
