4e2c2068395689f5306cb13e48440c9ca8211451
taha uygun <tahauygun@gmail.com>
2020-02-14 23:28:36 -0800

    setting authorization header

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -4,6 +4,7 @@ import ApolloClient from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {createHttpLink} from 'apollo-link-http';
 import {ApolloProvider} from '@apollo/react-hooks';
+import {setContext} from 'apollo-link-context';
 
 const httpLink = createHttpLink({
   uri:'http://localhost:5000'
@@ -12,6 +13,13 @@ const httpLink = createHttpLink({
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
+})
+
+const autLink = setContext(()=>{
+  const token = localStorage.getItem('jwtToken');
+  return{
+    headers: {Authorization:token ? `Bearer ${token}`: ''}
+  }
 })
 
 export default (
