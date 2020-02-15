4d79d7a0e08bbfec50d1248591f6fc713aa338a2
taha uygun <tahauygun@gmail.com>
2020-02-14 23:37:10 -0800

    fetching posts from query cache

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -14,8 +14,10 @@ function PostForm() {
     variables: values,
     update(proxy, result){
       proxy.readQuery({
-
+        query: FETCH_POSTS_QUERY
       })
+      data.getPosts = [result.data.getPost, ...data.getPosts];
+      proxy.writeQuery(({query: FETCH_POSTS_QUERY,data}))
       values.body = '';
     }
   })
