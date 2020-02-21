bce4a1426e9865402bb76da398b841427b0862e4
taha uygun <tahauygun@gmail.com>
2020-02-20 23:02:21 -0800

    adding like post func

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -5,14 +5,18 @@ import gql from 'graphql-tag';
 
 import {Button, Icon, Label} from "semantic-ui-react";
 
-function LikeButton({post:})
-
-<Button as='div' labelPosition='right' basic onClick={likePost}>
-  <Button color='teal'>
-    <Icon name='heart' />
-    Like
+function LikeButton({post: {id, likeCount, likes}}) {
+  <Button as='div' labelPosition='right' basic onClick={likePost}>
+    <Button color='teal'>
+      <Icon name='heart' />
+      Like
+    </Button>
+    <Label basic color='teal' pointing='left'>
+      {likeCount}
+    </Label>
   </Button>
-  <Label basic color='teal' pointing='left'>
-    {likeCount}
-  </Label>
-</Button>
+}
+
+
+
+export default LikeButton;
