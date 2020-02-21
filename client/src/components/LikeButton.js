e706176793ba438b03a6fcd7b51ecfa226033584
taha uygun <tahauygun@gmail.com>
2020-02-20 23:09:08 -0800

    finishing like func

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import {useMutation} from "@apollo/react-hooks";
 import gql from 'graphql-tag';
@@ -6,17 +6,53 @@ import gql from 'graphql-tag';
 import {Button, Icon, Label} from "semantic-ui-react";
 
 function LikeButton({post: {id, likeCount, likes}}) {
-  <Button as='div' labelPosition='right' basic onClick={likePost}>
-    <Button color='teal'>
+  const [liked, setLiked] = useState(false);
+  useEffect(()=>{
+    if (user && likes.find(like => like.user === user.username)) {
+      setLiked(true);
+    } else setLiked(false)
+    }, [user,likes]);
+
+  const [likePost] = useMutation(LIKE_POST_MUTATION,{
+    variables: {postId: id}
+  })
+
+  const likeButton = user ? (
+    liked ? (
+      <Button color='teal'>
+        <Icon name='heart' />
+      </Button>
+    ) : (
+      <Button color='teal' basic>
+        <Icon name='heart' />
+      </Button>
+    )
+  ) : (
+    <Button as={Link} to="/login" color='teal' basic>
       <Icon name='heart' />
-      Like
     </Button>
-    <Label basic color='teal' pointing='left'>
-      {likeCount}
-    </Label>
-  </Button>
+  )
+
+  return (
+    <Button as='div' labelPosition='right' basic onClick={likePost}>
+      {likeButton}
+      <Label basic color='teal' pointing='left'>
+        {likeCount}
+      </Label>
+    </Button>
+  )
 }
 
-
+const LIKE_POST_MUTATION = gql`
+    mutation likePost($postId: ID!){
+        likePost(postId: $postId){
+            id
+            likes{
+                id username
+            }
+            likeCount
+        }
+    }
+`
 
 export default LikeButton;
