6284a2a8b5384bcfa9e9c5b641028db4400b24f2
taha uygun <tahauygun@gmail.com>
2020-02-22 23:33:23 -0800

    adding single post route

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-
+import SinglePost from './pages/SinglePost';
 function App() {
   return (
     <AuthProvider>
@@ -20,6 +20,7 @@ function App() {
           <Router exact path="/" component={Home} />
           <AuthRoute exact path="/login" component={Login} />
           <AuthRoute exact path="/register" component={Register} />
+          <Route exact path="/posts/:postId" component={SinglePost}/>
         </Container>
       </Router>
     </AuthProvider>
