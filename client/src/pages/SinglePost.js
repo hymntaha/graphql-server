353e0f80d8139d3ed75d56da2319d27d036d5a7a
taha uygun <tahauygun@gmail.com>
2020-02-22 23:46:12 -0800

    adding some meta values

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,7 +1,8 @@
 import React from "react";
 import gql from "graphql-tag";
 import {useQuery} from "@apollo/react-hooks";
-import {Grid, GridColumn} from "semantic-ui-react";
+import {Grid, Image, Card} from "semantic-ui-react";
+import moment from "moment";
 
 function SinglePost(props) {
   const postId = props.match.params.postId;
@@ -23,7 +24,16 @@ function SinglePost(props) {
         <Grid.Row>
           <Grid.Column widht={2}>
             <Image src="https://react.semantic-ui.com/images/avatar/large/molly.png" size="small" float="right"/>
-
+          </Grid.Column>
+          <Grid.Column widht={10}>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>
+                  {username}
+                </Card.Header>
+                <Card.Meta>{moment(createdAt.fromNow())}</Card.Meta>
+              </Card.Content>
+            </Card>
           </Grid.Column>
         </Grid.Row>
       </Grid>
