7c004ef5f8c962d214b2d19ee3a2870bb4871b05
taha uygun <tahauygun@gmail.com>
2020-02-23 23:40:48 -0800

    adding delete button mutation

---
+++ client/src/components/DeleteButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import gql from 'graphql-tag';
+import {useMutation} from "@apollo/react-hooks";
+import {Button,Icon} from "semantic-ui-react";
+
+function DeleteButton(props) {
+  return (
+
+  )
+
+}
+
+export default DeleteButton
