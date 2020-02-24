e3d8ac2140cea27ba34f53278a63436dfc4740a6
taha uygun <tahauygun@gmail.com>
2020-02-23 23:46:33 -0800

    adding confirm model for deletion

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React,{useState} from "react";
 import gql from 'graphql-tag';
 import {useMutation} from "@apollo/react-hooks";
-import {Button,Icon} from "semantic-ui-react";
+import {Button,Icon, Confirm} from "semantic-ui-react";
 
 function DeleteButton(props) {
+  const [confirmOpen, setConfirmOpen] = useState(false);
+  const [deletePost] = useMutation(DELETE_POST_MUTATION,{
+    update(){
+
+    }
+    variables:{
+      postId
+    }
+  })
+
+
   return (
+    <>
     <Button as="div" color="red" floated="right" onClick={() => console.log('Delete post')}>
       <Icon name="trash" style={{margin:0}}/>
     </Button>
+    <Confirm open={confirmOpen} oncancel{()=> setConfirmOpen(false)} onConfirm={deletePost} />
+    </>
   );
 
 }
+
+const DELETE_POST_MUTATION =gql`
+    mutation deletePost($postId:ID!){
+        deletePost($postId: postId)
+    }
+`
 
 export default DeleteButton
