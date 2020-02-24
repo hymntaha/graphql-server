b7e7840cd67b574c441eb04ecd806672e81cbbf3
taha uygun <tahauygun@gmail.com>
2020-02-23 23:42:11 -0800

    finishing delete button mutation

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -5,8 +5,10 @@ import {Button,Icon} from "semantic-ui-react";
 
 function DeleteButton(props) {
   return (
-
-  )
+    <Button as="div" color="red" floated="right" onClick={() => console.log('Delete post')}>
+      <Icon name="trash" style={{margin:0}}/>
+    </Button>
+  );
 
 }
 
