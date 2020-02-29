6936831a08a045aee0dde34ca4bc91b14da07d28
taha uygun <tahauygun@gmail.com>
2020-02-28 22:53:38 -0800

    decoupling popup on like button

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -4,6 +4,7 @@ import {useMutation} from "@apollo/react-hooks";
 import gql from 'graphql-tag';
 
 import {Button, Icon, Label} from "semantic-ui-react";
+import MyPopup from "../util/MyPopup";
 
 function LikeButton({user, post: {id, likeCount, likes}}) {
   const [liked, setLiked] = useState(false);
@@ -35,7 +36,9 @@ function LikeButton({user, post: {id, likeCount, likes}}) {
 
   return (
     <Button as='div' labelPosition='right' basic onClick={likePost}>
-      {likeButton}
+      <MyPopup content={liked ? 'Unlike': 'Like'}>
+        {likeButton}
+      </MyPopup>
       <Label basic color='teal' pointing='left'>
         {likeCount}
       </Label>
